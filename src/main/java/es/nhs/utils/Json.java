fa0d36abf6f7e59618da2c271085e2f9a652d010
nhsdam <nhersal281@g.educaand.es>
2022-11-28 12:52:27 +0100

    Plantilla, por hacer models

---
+++ src/main/java/es/nhs/utils/Json.java
@@ -0,0 +1,35 @@
+package es.nhs.utils;
+
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+public class Json
+{
+    private static ObjectMapper mapper;
+
+    public static ObjectMapper mapper()
+    {
+        if (Json.mapper == null)
+        {
+            Json.mapper = Json.createJson();
+        }
+        return Json.mapper;
+    }
+
+    private static ObjectMapper createJson()
+    {
+        return new ObjectMapper();
+    }
+
+//    private static ObjectMapper createJson()
+//    {
+//        final ObjectMapper mapper = new ObjectMapper();
+//        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+//        mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, true);
+//        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, true);
+//        return mapper;
+//    }
+
+}
